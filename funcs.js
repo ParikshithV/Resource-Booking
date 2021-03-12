0952e89191ffbfd7da191114a159f85365c5682e
ParikshithV <parikv99@gmail.com>
2021-03-12 14:35:14 +0530

    booking page js changes

--- funcs.js
+++ funcs.js
@@ -2,7 +2,7 @@ function submitFunc(form){
   var flag = 0;
   var items = ["Hand mic", "Audio Console", "Speakers", "Projector"];
   var date = document.getElementById("date").value;
-  var alertMsg = ("You have selected\n");
+  var alertMsg = ("You have selected following equipment: \n");
   var equipment = [document.getElementById("equipment1").checked,
                   document.getElementById("equipment2").checked,
                   document.getElementById("equipment3").checked,
@@ -10,12 +10,15 @@ function submitFunc(form){
   var len = equipment.length;
   for(i=0 ; i<len ; i++){
     if (equipment[i]==true){
-      alertMsg = alertMsg.concat(items[i]);
+      alertMsg = (alertMsg.concat(items[i])+"\n");
       flag = 1;
     }
   }
+
+  alertMsg = alertMsg.concat("\nFor "+date);
+
   if(flag==1){
-    alert(alertMsg+"\n");
+    alert(alertMsg);
   }
   else{
     alert("No equipment choosen");
